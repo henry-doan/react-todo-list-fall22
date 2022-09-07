887fe54b587995179653cb428f91a2ca3cb4ec07
Henry Doan <henrydoan96@gmail.com>
2022-09-06 21:00:59 -0600

    end lecture

---
+++ src/components/todos/Todo.js
@@ -0,0 +1,14 @@
+
+const Todo = ({ id, title, complete }) => {
+
+  return (
+    // <li key={id}>
+    <li
+      style={{ textDecoration: complete ?  'line-through' : 'none' }}
+    >
+      {title}
+    </li>
+  )
+}
+
+export default Todo;
